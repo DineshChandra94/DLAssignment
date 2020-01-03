83579cbe95350c083a72f19b29dc7f8a3e00c8d2
Dinesh Chandra <DC44819@CORP.CYIENT.COM>
2020-01-04 03:00:02 +0530

    commit

---
+++ training.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from Custom import DetectionModelTrainer
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+trainer.setDataDirectory(data_directory=os.path.join(BASE_DIR, "data"))
+trainer.setTrainConfig(object_names_array= os.path.join(BASE_DIR,'number_to_type.json'), batch_size=16, num_experiments=30, train_from_pretrained_model=os.path.join(BASE_DIR,"pretrained-yolov3.h5"))
+trainer.trainModel()
+    
