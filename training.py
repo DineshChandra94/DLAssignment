f873752769383df4bb172b90bdb1b07d6d2f3218
Dinesh Chandra <DC44819@CORP.CYIENT.COM>
2020-01-04 03:37:14 +0530

    commit

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 import sys
 from Custom import DetectionModelTrainer
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -7,6 +8,6 @@ sys.path.append(BASE_DIR)
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory=os.path.join(BASE_DIR, "data"))
-trainer.setTrainConfig(class_json_path = os.path.join(BASE_DIR,'number_to_type.json'), batch_size=16, num_experiments=30, train_from_pretrained_model=os.path.join(BASE_DIR,"pretrained-yolov3.h5"))
+trainer.setTrainConfig(class_json_path = os.path.join(BASE_DIR,'number_to_type.json'), batch_size=8, num_experiments=30, train_from_pretrained_model=os.path.join(BASE_DIR,"pretrained-yolov3.h5"))
 trainer.trainModel()
     
