b724ba491050ccf8dc7c37871f51df2eb717e4c0
Dinesh Chandra <DC44819@CORP.CYIENT.COM>
2020-01-05 14:39:57 +0530

    commit

--- testing.py
+++ testing.py
@@ -5,6 +5,6 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("/content/DLAssignment/custom_model.h5")
 detector.setJsonPath("/content/DLAssignment/data/json/detection_config.json")
 detector.loadModel()
-
+detections = detector.detectObjectsFromImage(input_image="/content/DLAssignment/data/validation/images/27.jpg", output_image_path="/content/DLAssignment/27.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
